70c1eda6187ef1859f92a2931945b58b13547858
Romas Tony P <94161293+romastony0@users.noreply.github.com>
2024-05-31 09:59:38 +0530

    Update index.php

--- index.php
+++ index.php
@@ -148,7 +148,7 @@ function processExcel($inputFile, $accessToken, $conn, $selectedOperator)
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <link rel="icon" type="image/x-icon" href="./icons8-url-50.png">
-    <title>Bitly URL Shortener Hello</title>
+    <title>Bitly URL Shortener</title>
     <!-- Bootstrap CSS -->
     <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.1.3/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-1BmE4kWBq78iYhFldvKuhfTAU6auU8tT94WrHftjDbrCEXSU1oBoqyl2QvZ6jIW3" crossorigin="anonymous">
     <style>
